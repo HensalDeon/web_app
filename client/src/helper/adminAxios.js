0802d6990cfa4869e1f280d4e9c73bdbcea45c9e
hensal <hensaldeon777@gmail.com>
2023-11-11 12:26:52 +0530

    build

--- client/src/helper/adminAxios.js
+++ client/src/helper/adminAxios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { Promise } from "es6-promise";
 
-const baseURL = "http://localhost:8080/admin";
+const baseURL = "http://localhost:8080/api/admin";
 
 /** Make API Requests */
 
--- server/server.js
+++ server/server.js
@@ -1,40 +1,47 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 import morgan from "morgan";
 import connect from "./database/connection.js";
 import userRouter from "./routes/userRouter.js";
 import adminRouter from "./routes/adminRouter.js";
 import bodyParser from "body-parser";
 import ENV from "./config.js";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
 /** middlewears */
 app.use(express.json());
+app.use(express.static(path.join(__dirname, "./build")));
 app.use(cors());
 app.use(morgan("tiny"));
-app.use(bodyParser.json({ limit: '5mb' }));
+app.use(bodyParser.json({ limit: "5mb" }));
 app.disable("x-powered-by"); //less hackers know about our stack
 
-
 /** HTTP GET request */
-app.get("/", (req, res) => {
-    res.status(201).json("Home GET request");
+app.get(/^(?!\/api).+/, (req, res) => {
+    res.sendFile(path.join(__dirname,"./build/index.html"))
 });
 
 /** api routes */
-app.use('/', userRouter);
-app.use('/admin', adminRouter);
+app.use("/api/", userRouter);
+app.use("/api/admin", adminRouter);
 
 /** start server when only we have a valid connection*/
-connect().then(() => {
-    try {
-        app.listen(ENV.PORT, () => {
-            console.log(`Server connected to http://localhost:${ENV.PORT}`);
-        });
-    } catch (error) {
-        console.log("Cannot connect to the server");
-    }
-}).catch(error =>{
-    console.log("Inavlid database connection");
-})
+connect()
+    .then(() => {
+        try {
+            app.listen(ENV.PORT, () => {
+                console.log(`Server connected to http://localhost:${ENV.PORT}`);
+            });
+        } catch (error) {
+            console.log("Cannot connect to the server");
+        }
+    })
+    .catch((error) => {
+        console.log("Inavlid database connection");
+    });
